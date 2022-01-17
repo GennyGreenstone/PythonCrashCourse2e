3285786f85615a5cccc14a56cb06a72410628b13
PigQueenGen <71272822+PigQueenGen@users.noreply.github.com>
2022-01-16 20:34:27 -0600

    Add files via upload

---
+++ exercise_eleven_animals.py
@@ -0,0 +1,6 @@
+## 3-11. Intentional Error: IF you haven't had an index error yet, make one happen. Correct the error before closing the program
+
+animals = ['cat', 'dog', 'pig', 'bird']
+print(animals[3])
+
+#Index error created: print(animals[4])
